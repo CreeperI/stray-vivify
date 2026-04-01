3947be815dbdbd56c4488fffdb79466ea0449af4
CreeperI <Creeper_001@163.com>
2026-04-01 17:42:52 +0800

    let the tools able to refresh the chart

--- src/renderer/src/components/chart-v2/chart-tabs/small/fn-edit-tools.vue
+++ src/renderer/src/components/chart-v2/chart-tabs/small/fn-edit-tools.vue
@@ -1,31 +1,50 @@
 <script lang="ts" setup>
 import { GlobalStat } from '@renderer/core/globalStat'
 import { EditorTools } from '@renderer/core/chart/tools'
-
+import { ref } from 'vue'
+import ANumberInput from '@renderer/components/a-elements/a-number-input.vue'
+import ACheckbox from '@renderer/components/a-elements/a-checkbox.vue'
 const selectedNID = GlobalStat.NoteClipboard.selected
-
+const toolMultiArgs = ref({
+    multiVal: 1,
+    baseZero: false
+})
+console.log(toolMultiArgs.value)
 </script>
 
 <template>
-    <div class="edit-frame" v-if="selectedNID.length > 0">
-        <p style="text-align: center;">想做些什么呢？</p>
+    <div v-if="selectedNID.length > 0" class="edit-frame">
+        <p style="text-align: center">想做些什么呢？</p>
         <div class="edit-button-frame">
-            <div class="edit-button" @click="EditorTools.test(selectedNID)">Time Randomize</div>
-            <div class="edit-button" @click="EditorTools.mirror(selectedNID)">Mirror</div>
+            <div class="edit-button ln1" @click="EditorTools.test(selectedNID)">Time Randomize</div>
+            <div class="edit-button ln1" @click="EditorTools.mirror(selectedNID)">Mirror</div>
+            <div class="edit-button ln2"
+                @click="EditorTools.mutiplier(selectedNID, toolMultiArgs.multiVal, toolMultiArgs.baseZero)">TimeMultiply
+            </div>
+            <a-number-input class=" ln2" v-model="toolMultiArgs.multiVal" :min="0.1" :step="0.1" />
+            <label class=" ln2" style="grid-column: span 1">0ms base</label>
+            <a-checkbox v-model="toolMultiArgs.baseZero" class="ln2" style="grid-column: span 1" />
         </div>
     </div>
-
 </template>
 
 <style lang="css" scoped>
 .edit-frame {
-    padding-top: 0px
+    padding-top: 0px;
+}
+
+.ln1 {
+    grid-column: span 3;
+}
+
+.ln2 {
+    grid-column: span 2;
 }
 
 .edit-button-frame {
     display: grid;
     grid-template-rows: 1fr 1fr;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(6, 1fr);
     gap: 10px;
 }
 
--- src/renderer/src/core/chart/tools.ts
+++ src/renderer/src/core/chart/tools.ts
@@ -1,14 +1,14 @@
 import { ChartTypeV2 } from '@preload/chart-types'
 import { Chart } from '@renderer/core/chart/chart'
-
+import { RefreshAll } from '@renderer/core/misc/refresh-all'
 
 export const EditorTools = {
     test: test,
-    mirror: mirror
+    mirror: mirror,
+    mutiplier: timeMultiplier
 }
 
 function test(nids: number[]) {
-    console.log("test triggered!")
     const notes: ChartTypeV2.note[] = nids.map((nid) => Chart.$current.diff.to_note(nid))
     let maxTime = -1
     let minTime = 2147483647
@@ -19,6 +19,7 @@ function test(nids: number[]) {
     notes.forEach((note) => {
         note.time = Math.round(Math.random() * (maxTime - minTime) + minTime)
     })
+    RefreshAll.fuck()
 }
 
 function mirror(nids: number[]) {
@@ -27,4 +28,21 @@ function mirror(nids: number[]) {
         const midLane = (4 - note.width) / 2
         note.lane = Math.round(-(note.lane - midLane) + midLane)
     })
+    RefreshAll.fuck()
+}
+
+function timeMultiplier(nids: number[], multiplier: number, baseOnZero: boolean) {
+    let minTime = 2147483647
+    const notes: ChartTypeV2.note[] = nids.map((nid) => Chart.$current.diff.to_note(nid))
+    notes.forEach((note) => {
+        minTime = Math.min(minTime, note.time)
+    })
+    const baseTime = baseOnZero ? 0 : minTime
+    notes.forEach((note) => {
+        note.time = Math.round((note.time - baseTime) * multiplier + baseTime)
+        if ('len' in note) {
+            note.len = Math.round(note.len * multiplier)
+        }
+    })
+    RefreshAll.fuck()
 }
