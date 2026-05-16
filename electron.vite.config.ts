0644a13b1e3d738940d5785336fa728528776d28
Ts-Final <139440186+Ts-Final@users.noreply.github.com>
2026-05-16 22:20:01 +0800

    fuck @preload missing error

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@preload': resolve('src/preload')
       }
     },
     plugins: plugins
